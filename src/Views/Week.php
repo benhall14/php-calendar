ff9316c1e63b0eb0282ef83fe3edc92080c85c34
GeoSot <geo.sotis@gmail.com>
2024-10-16 20:00:08 +0300

    ref: break week-view to methods

--- src/Views/Week.php
+++ src/Views/Week.php
@@ -9,9 +9,15 @@ use benhall14\phpCalendar\Event;
 use Carbon\Carbon;
 use Carbon\CarbonInterface;
 use Carbon\CarbonInterval;
+use Carbon\CarbonPeriod;
 
 class Week extends View
 {
+    /**
+     * @var list<Event>
+     */
+    protected array $usedEvents = [];
+
     protected function findEvents(CarbonInterface $start, CarbonInterface $end): array
     {
         $callback = fn (Event $event): bool => $event->start->betweenIncluded($start, $end)
@@ -22,11 +28,32 @@ class Week extends View
     }
 
     /**
+     * Returns the calendar output as a week view.
+     */
+    public function render(DateTimeInterface|string|null $startDate = null, string $color = ''): string
+    {
+        $startDate = $this->sanitizeStartDate(Carbon::parse($startDate));
+        $carbonPeriod = $startDate->locale($this->config->locale)->toPeriod(7);
+        $calendar = [
+            '<div class="weekly-calendar-container">',
+            '<table class="weekly-calendar calendar '.$color.' '.$this->config->table_classes.'">',
+            $this->makeHeader($carbonPeriod),
+            '<tbody>',
+            $this->renderBlocks($carbonPeriod),
+            '</tbody>',
+            '</table>',
+            '</div>',
+        ];
+
+        return implode('', $calendar);
+    }
+
+    /**
      * Get an array of time slots.
      *
      * @return list<string>
      */
-    public function getTimes(): array
+    protected function getTimes(): array
     {
         $start_time = Carbon::createFromFormat('H:i', $this->config->start_time);
         $end_time = Carbon::createFromFormat('H:i', $this->config->end_time);
@@ -34,7 +61,8 @@ class Week extends View
             $end_time->addDay();
         }
 
-        $carbonPeriod = CarbonInterval::minutes($this->config->time_interval)->toPeriod($this->config->start_time, $end_time);
+        $carbonPeriod = CarbonInterval::minutes($this->config->time_interval)->toPeriod($this->config->start_time,
+            $end_time);
 
         $times = [];
         foreach ($carbonPeriod->toArray() as $carbon) {
@@ -44,22 +72,8 @@ class Week extends View
         return array_unique($times);
     }
 
-    /**
-     * Returns the calendar output as a week view.
-     */
-    public function render(DateTimeInterface|string|null $startDate = null, string $color = ''): string
+    protected function sanitizeStartDate(CarbonInterface $startDate): CarbonInterface
     {
-        $calendar = '<div class="weekly-calendar-container">';
-
-        $colspan = 7;
-
-        foreach (array_intersect($this->config->hiddenDays, Carbon::getDays()) as $day) {
-            --$colspan;
-            $calendar .= '<style>.cal-'.strtolower($day).',.cal-day-'.strtolower($day).'{display:none!important;}</style>';
-        }
-
-        $startDate = Carbon::parse($startDate);
-
         if ($this->config->starting_day !== $startDate->dayOfWeek) {
             if (0 === $this->config->starting_day) {
                 $startDate->previous('sunday');
@@ -68,94 +82,100 @@ class Week extends View
             }
         }
 
-        $carbonPeriod = $startDate->locale($this->config->locale)->toPeriod(7);
+        return $startDate;
+    }
 
-        $today = Carbon::now();
+    protected function makeHeader(CarbonPeriod $carbonPeriod): string
+    {
+        $headerString = '<thead>';
 
-        $calendar .= '<table class="weekly-calendar calendar '.$color.' '.$this->config->table_classes.'">';
+        $headerString .= '<tr class="calendar-header">';
 
-        $calendar .= '<thead>';
-
-        $calendar .= '<tr class="calendar-header">';
-
-        $calendar .= '<th></th>';
+        $headerString .= '<th></th>';
 
         /* @var Carbon $date */
         foreach ($carbonPeriod->toArray() as $carbon) {
-            $calendar .= '<th class="cal-th cal-th-'.strtolower($carbon->englishDayOfWeek).'">';
-            $calendar .= '<div class="cal-weekview-dow">'.ucfirst($carbon->localeDayOfWeek).'</div>';
-            $calendar .= '<div class="cal-weekview-day">'.$carbon->day.'</div>';
-            $calendar .= '<div class="cal-weekview-month">'.ucfirst($carbon->localeMonth).'</div>';
-            $calendar .= '</th>';
+            if ($this->config->dayShouldBeHidden($carbon)) {
+                continue;
+            }
+            $headerString .= '<th class="cal-th cal-th-'.strtolower($carbon->englishDayOfWeek).'">';
+            $headerString .= '<div class="cal-weekview-dow">'.ucfirst($carbon->localeDayOfWeek).'</div>';
+            $headerString .= '<div class="cal-weekview-day">'.$carbon->day.'</div>';
+            $headerString .= '<div class="cal-weekview-month">'.ucfirst($carbon->localeMonth).'</div>';
+            $headerString .= '</th>';
         }
 
-        $calendar .= '</tr>';
+        $headerString .= '</tr>';
 
-        $calendar .= '</thead>';
+        return $headerString.'</thead>';
+    }
 
-        $calendar .= '<tbody>';
-
-        $used_events = [];
-
+    protected function renderBlocks(CarbonPeriod $carbonPeriod): string
+    {
+        $today = Carbon::now();
+        $content = '';
         foreach ($this->getTimes() as $time) {
-            $calendar .= '<tr>';
+            $content .= '<tr>';
 
             $start_time = $time;
             $end_time = date('H:i', strtotime($time.' + '.$this->config->time_interval.' minutes'));
 
-            $calendar .= '<td class="cal-weekview-time-th"><div>'.$start_time.' - '.$end_time.'</div></td>';
+            $content .= '<td class="cal-weekview-time-th"><div>'.$start_time.' - '.$end_time.'</div></td>';
 
             foreach ($carbonPeriod->toArray() as $carbon) {
+                if ($this->config->dayShouldBeHidden($carbon)) {
+                    continue;
+                }
+
                 $datetime = $carbon->setTimeFrom($time);
 
                 $events = $this->findEvents($datetime, $datetime->clone()->addMinutes($this->config->time_interval));
 
                 $today_class = $carbon->isSameHour($today) ? ' today' : '';
 
-                $calendar .= '<td class="cal-weekview-time '.$today_class.'">';
+                $content .= '<td class="cal-weekview-time '.$today_class.'">';
 
-                $calendar .= '<div>';
+                $content .= '<div>';
 
                 foreach ($events as $event) {
-                    $classes = '';
-
-                    if (in_array($event, $used_events)) {
-                        $event_summary = '&nbsp;';
-                    } else {
-                        $event_summary = $event->summary;
-                        $used_events[] = $event;
-                    }
-
-                    // is the current day the start of the event
-                    if ($event->start->isSameDay($carbon)) {
-                        $classes .= $event->mask ? ' mask-start' : '';
-                        $classes .= $event->classes;
-                    // is the current day in between the start and end of the event
-                    } elseif ($carbon->betweenExcluded($event->start, $event->end)) {
-                        $classes .= $event->mask ? ' mask' : '';
-
-                    // is the current day the start of the event
-                    } elseif ($carbon->isSameDay($event->end)) {
-                        $classes .= $event->mask ? ' mask-end' : '';
-                    }
-
-                    $calendar .= '<div class="cal-weekview-event '.$classes.'">';
-                    $calendar .= $event_summary;
-                    $calendar .= '</div>';
+                    $content .= $this->renderEvent($event, $carbon);
                 }
 
-                $calendar .= '</div>';
+                $content .= '</div>';
 
-                $calendar .= '</td>';
+                $content .= '</td>';
             }
 
-            $calendar .= '<tr/>';
+            $content .= '<tr/>';
         }
 
-        $calendar .= '</tbody>';
+        return $content;
+    }
 
-        $calendar .= '</table>';
+    protected function renderEvent(Event $event, CarbonInterface $dateTime): string
+    {
+        $classes = '';
 
-        return $calendar.'</div>';
+        if (in_array($event, $this->usedEvents)) {
+            $eventSummary = '&nbsp;';
+        } else {
+            $eventSummary = $event->summary;
+            $this->usedEvents[] = $event;
+        }
+
+        // is the current day the start of the event
+        if ($event->start->isSameDay($dateTime)) {
+            $classes .= $event->mask ? ' mask-start' : '';
+            $classes .= $event->classes;
+        // is the current day in between the start and end of the event
+        } elseif ($dateTime->betweenExcluded($event->start, $event->end)) {
+            $classes .= $event->mask ? ' mask' : '';
+
+        // is the current day the start of the event
+        } elseif ($dateTime->isSameDay($event->end)) {
+            $classes .= $event->mask ? ' mask-end' : '';
+        }
+
+        return '<div class="cal-weekview-event '.$classes.'">'.$eventSummary.'</div>';
     }
 }
